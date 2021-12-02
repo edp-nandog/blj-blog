06b22a6e246f1e4acb377cdf2050349c7b40ea31
edp-nandog <ngmuender@edp.ch>
2021-12-02 15:18:15 +0100

    alles Fertig

--- info.php
+++ info.php
@@ -29,7 +29,6 @@
 
     <!-- Verbidnug für die Links der anderen -->
     <?php
-
     $pdo = new PDO('mysql:host=mysql2.webland.ch;dbname=d041e_dagomez', 'd041e_dagomez', '54321_Db!!!', [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
